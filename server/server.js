98321ab51f9952e5b137d7a6ccd09fc70bc4b97b
ANMOL GOYAL <anmolgoyal1974@gmail.com>
2026-03-29 13:42:08 +0530

    fix cors headers final

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
-dotenv.config();
-connectDB();
-
-const app = express();
+const cors = require("cors");
 
 app.use(cors({
-  origin: "https://student-dashboard-ashy-rho.vercel.app",
+  origin: ["https://student-dashboard-ashy-rho.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", (req, res) => res.sendStatus(200));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://student-dashboard-ashy-rho.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
